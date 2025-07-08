0f3c8e59bd3e7e29e16fa794f6e3554f9491225a
aryan7424 <aryan@branddy.net>
2025-07-09 01:19:19 +0530

    initialized next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Hello, Flight Booker!</h1>
+    </div>
+  );
+}
